214f513d7316c04fc1c309cb5d62ef1cb1584b3b
Violaolaola98 <168519256+Violaolaola98@users.noreply.github.com>
2024-05-14 12:33:05 +0700

    Add files via upload

---
+++ Js_Praktikum/coba.js
@@ -0,0 +1,19 @@
+let x=10+6
+    x+=15+25 
+    x!= 24+13
+console.log(x)
+
+let y = 15+x
+    y +=24+13
+console.log(y)
+
+var w=10+7
+console.log(w)
+var d=7+5
+console.log(d)
+var k=27+6
+console.log(k)
+var i=4+8
+console.log(i)
+var o=25+6
+console.log(i)
---
+++ Js_Praktikum/main.js
@@ -0,0 +1,22 @@
+var x="nian";
+x+=17;
+console.log(x)
+var x="dita";
+x+=17;
+console.log(x)
+var x="aulia";
+x+=17;
+console.log(x)
+var x="nanda";
+x+=17;
+console.log(x)
+var x="jukin";
+x+=17;
+console.log(x)
+
+var x={nama:"ola", umur:"16", kelas:"RPL3", absen:"35"}
+console.log(x)
+console.log(x.nama)
+console.log(x.kelas)
+console.log(x.umur)
+console.log(x.absen)
